113922128bb8c187d1ef83abaef5b4a5f1a810b0
prince91221 <prince.sharma930@gmail.com>
2021-03-12 01:50:59 +0530

    changed URL in request.open()

--- custom.js
+++ custom.js
@@ -1,12 +1,12 @@
 var request = new XMLHttpRequest();
-request.open('GET', 'weather.json', 'true');
+request.open('GET', 'http://jsoneditoronline.org/#left=local.taqedu&right=local.wibubo', 'true');
 request.onload = function() {
     
     //Accessing JSON data below
     var data = JSON.parse(this.response);
 
     if (request.status >= 200 && request.status < 400) {
-        data.forEach((openweatherapp) => {  //error on this line.
+        data.forEach((openweatherapp) => {
             console.log(openweatherapp.weather);
         })
     } else {
